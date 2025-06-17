a6efede6c1805849635d01a3723bfad35d79502f
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 <fabio_inacio@hotmail.com>
2025-06-17 11:47:14 -0300

    SPRING BOOT 3 4 - REQUISIÇÕES GET

---
+++ src/main/java/med/voll/api/medico/DadosListagemMedicos.java
@@ -0,0 +1,19 @@
+package med.voll.api.medico;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosListagemMedicos(
+        String nome,
+        String email,
+        String crm,
+        Especialidade especialidade) {
+
+    public DadosListagemMedicos(Medico medico) {
+        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+}
